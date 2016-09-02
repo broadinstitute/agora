2bd6d662b85a4cf88381411a1da52144dc5cd838
Sarah Walker <skwalker@broadinstitute.org>
2016-09-02 16:39:44 -0400

    actually added test

---
+++ src/test/scala/org/broadinstitute/dsde/agora/server/webservice/AgoraServiceStatusSpec.scala
@@ -0,0 +1,39 @@
+package org.broadinstitute.dsde.agora.server.webservice
+
+import spray.http.StatusCodes
+
+class AgoraServiceStatusSpec extends ApiServiceSpec {
+
+  override def beforeAll() = {
+    ensureDatabasesAreRunning()
+  }
+
+  override def afterAll() = {
+    clearDatabases()
+  }
+
+  // our test sql db is H2, which doesn't allow us to check for version
+  "Agora" should "should not actually be able to test the sql db" in {
+    Get(s"/status") ~> apiStatusService.statusRoute ~>
+      check {
+        assertResult(StatusCodes.InternalServerError) {
+          status
+        }
+        assertResult(s"""{"status": "down", "error": "Function "VERSION" not found; SQL statement:\nselect version(); [90022-175]"}""") {
+          responseAs[String]
+        }
+      }
+  }
+
+  ignore should "run and connect to DBs" in {
+    Get(s"/status") ~> apiStatusService.statusRoute ~>
+      check {
+        assertResult(StatusCodes.OK) {
+          status
+        }
+        assertResult(s"""{"status": "up"}""") {
+          responseAs[String]
+        }
+      }
+  }
+}

a980ec0a35b3617e2ef6368fb280771efb456cb1
Hussein Elgridly <hussein@broadinstitute.org>
2017-03-31 18:06:30 -0400

    nsperms spec

--- src/test/scala/org/broadinstitute/dsde/agora/server/dataaccess/permissions/NamespacePermissionsClientSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/agora/server/dataaccess/permissions/NamespacePermissionsClientSpec.scala
@@ -2,29 +2,27 @@ package org.broadinstitute.dsde.agora.server.dataaccess.permissions
 
 import org.broadinstitute.dsde.agora.server.AgoraTestData._
 import org.broadinstitute.dsde.agora.server.AgoraTestFixture
-import org.broadinstitute.dsde.agora.server.business.{AgoraBusiness, PermissionBusiness}
 import org.broadinstitute.dsde.agora.server.dataaccess.permissions.AgoraPermissions._
-import org.broadinstitute.dsde.agora.server.dataaccess.permissions.NamespacePermissionsClient._
 import org.broadinstitute.dsde.agora.server.exceptions.PermissionModificationException
 import org.broadinstitute.dsde.agora.server.model.AgoraEntity
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, DoNotDiscover, FlatSpec}
 
+import scala.concurrent.Future
+
 @DoNotDiscover
 class NamespacePermissionsClientSpec extends FlatSpec with ScalaFutures with BeforeAndAfterAll with AgoraTestFixture {
 
-  var agoraBusiness: AgoraBusiness = _
-  var permissionBusiness: PermissionBusiness = _
   var testBatchPermissionEntityWithId: AgoraEntity = _
 
   override def beforeAll(): Unit = {
     ensureDatabasesAreRunning()
-    agoraBusiness = new AgoraBusiness()
-    permissionBusiness = new PermissionBusiness()
-    agoraBusiness.insert(testEntity1, mockAuthenticatedOwner.get)
-    agoraBusiness.insert(testEntity2, mockAuthenticatedOwner.get)
-    agoraBusiness.insert(testEntity3, mockAuthenticatedOwner.get)
-    testBatchPermissionEntityWithId = agoraBusiness.find(testEntity3, None, Seq(testEntity3.entityType.get), mockAuthenticatedOwner.get).head
+    patiently(Future.sequence(Seq(
+      agoraBusiness.insert(testEntity1, mockAuthenticatedOwner.get),
+      agoraBusiness.insert(testEntity2, mockAuthenticatedOwner.get),
+      agoraBusiness.insert(testEntity3, mockAuthenticatedOwner.get)
+    )))
+    testBatchPermissionEntityWithId = patiently(agoraBusiness.find(testEntity3, None, Seq(testEntity3.entityType.get), mockAuthenticatedOwner.get)).head
   }
 
   override def afterAll(): Unit = {
@@ -32,18 +30,26 @@ class NamespacePermissionsClientSpec extends FlatSpec with ScalaFutures with Bef
   }
 
   "Agora" should "add namespace permissions." in {
-    val insertCount1 = insertNamespacePermission(testEntity1, AccessControl(testEntity1.owner.get, AgoraPermissions(All)))
-    val insertCount2 = insertNamespacePermission(testEntity2, AccessControl(testEntity2.owner.get, AgoraPermissions(All)))
+    val insertCount1 = runInDB { db =>
+      db.nsPerms.insertNamespacePermission(testEntity1, AccessControl(testEntity1.owner.get, AgoraPermissions(All)))
+    }
+    val insertCount2 = runInDB { db =>
+      db.nsPerms.insertNamespacePermission(testEntity2, AccessControl(testEntity2.owner.get, AgoraPermissions(All)))
+    }
     assert(insertCount1 === 1)
     assert(insertCount2 === 1)
   }
 
   "Agora" should "should silently add a user to the db if not already there." in {
-    insertNamespacePermission(testEntity1, AccessControl(owner3.get, AgoraPermissions(All)))
+    runInDB { db =>
+      db.nsPerms.insertNamespacePermission(testEntity1, AccessControl(owner3.get, AgoraPermissions(All)))
+    }
   }
 
   "Agora" should "return namespace permissions for authorized users." in {
-    val permissions = getNamespacePermission(testEntity1, testEntity1.owner.get)
+    val permissions = runInDB { db =>
+      db.nsPerms.getNamespacePermission(testEntity1, testEntity1.owner.get)
+    }
     assert(permissions.canManage)
     assert(permissions.canCreate)
     assert(permissions.canRead)
@@ -53,7 +59,9 @@ class NamespacePermissionsClientSpec extends FlatSpec with ScalaFutures with Bef
 
   "Agora" should "allow creation of namespaces that do not exist" in {
     val testEntityNewNamespace = testEntity1.copy(namespace = Option("unused_namepace2"))
-    val permissions = getNamespacePermission(testEntityNewNamespace, testEntity2.owner.get)
+    val permissions = runInDB { db =>
+      db.nsPerms.getNamespacePermission(testEntityNewNamespace, testEntity2.owner.get)
+    }
     assert(permissions.canCreate)
     assert(!permissions.canManage)
     assert(!permissions.canRead)
@@ -62,7 +70,9 @@ class NamespacePermissionsClientSpec extends FlatSpec with ScalaFutures with Bef
   }
 
   "Agora" should "give no permissions when namespace exists and user is unauthorized" in {
-    val permissions = getNamespacePermission(testEntity2, testEntity1.owner.get)
+    val permissions = runInDB { db =>
+      db.nsPerms.getNamespacePermission(testEntity2, testEntity1.owner.get)
+    }
     assert(!permissions.canCreate)
     assert(!permissions.canManage)
     assert(!permissions.canRead)
@@ -71,10 +81,14 @@ class NamespacePermissionsClientSpec extends FlatSpec with ScalaFutures with Bef
   }
 
   "Agora" should "edit namespace permissions" in {
-    val editCount = editNamespacePermission(testEntity2, AccessControl(testEntity2.owner.get, AgoraPermissions(Redact)))
+    val editCount = runInDB { db =>
+      db.nsPerms.editNamespacePermission(testEntity2, AccessControl(testEntity2.owner.get, AgoraPermissions(Redact)))
+    }
     assert(editCount == 1)
 
-    val permissions = getNamespacePermission(testEntity2, testEntity2.owner.get)
+    val permissions = runInDB { db =>
+      db.nsPerms.getNamespacePermission(testEntity2, testEntity2.owner.get)
+    }
     assert(permissions.canRedact)
     assert(!permissions.canManage)
     assert(!permissions.canCreate)
@@ -83,7 +97,9 @@ class NamespacePermissionsClientSpec extends FlatSpec with ScalaFutures with Bef
   }
 
   "Agora" should "delete namespace permissions" in {
-    val deleteCount = deleteNamespacePermission(testEntity2, testEntity2.owner.get)
+    val deleteCount = runInDB { db =>
+      db.nsPerms.deleteNamespacePermission(testEntity2, testEntity2.owner.get)
+    }
     assert(deleteCount == 1)
   }
 

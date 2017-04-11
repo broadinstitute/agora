70af3d2c7a38a806b7cc9ebb9bc78193544da921
Hussein Elgridly <hussein@broadinstitute.org>
2017-04-11 15:18:39 -0400

    pass cause up to permission exc

--- src/main/scala/org/broadinstitute/dsde/agora/server/exceptions/PermissionNotFoundException.scala
+++ src/main/scala/org/broadinstitute/dsde/agora/server/exceptions/PermissionNotFoundException.scala
@@ -2,6 +2,6 @@ package org.broadinstitute.dsde.agora.server.exceptions
 
 import com.typesafe.scalalogging.slf4j.LazyLogging
 
-case class PermissionNotFoundException(message: String = "Could not find permissions.", ex: Throwable = null) extends Exception with LazyLogging {
-  override def getMessage: String = message
+case class PermissionNotFoundException(message: String = "Could not find permissions.", ex: Throwable = null) extends Exception(message, ex) with LazyLogging {
+  //override def getMessage: String = message
 }

8da91be57f8e49b7a66869d9153b5b1e11553993
David An <davidan@genomebridge.org>
2017-08-31 12:06:55 -0400

    cleaner error message for NotFound (#207)

--- src/main/scala/org/broadinstitute/dsde/agora/server/exceptions/AgoraEntityNotFoundException.scala
+++ src/main/scala/org/broadinstitute/dsde/agora/server/exceptions/AgoraEntityNotFoundException.scala
@@ -4,8 +4,10 @@ import org.broadinstitute.dsde.agora.server.model.AgoraEntity
 
 case class AgoraEntityNotFoundException(agoraEntity: AgoraEntity) extends Exception {
   override def getMessage: String = {
-    s"Entity ${agoraEntity.namespace.getOrElse("No Namespace")}" +
-      s".${agoraEntity.name.getOrElse("No Name")}" +
-      s".${agoraEntity.snapshotId.getOrElse(-1)} not found."
+    val namespaceStr = agoraEntity.namespace.getOrElse("[namespace unknown]")
+    val nameStr = agoraEntity.name.getOrElse("[name unknown]")
+    val snapshotStr = agoraEntity.snapshotId.map("/"+_).getOrElse("")
+
+    s"Methods Repository entity $namespaceStr/$nameStr$snapshotStr not found."
   }
 }

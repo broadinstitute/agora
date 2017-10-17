6f3fd41cad07855b56b3401309ed7afb4bd60a7b
Greg Rushton <grushton@broadinstitute.org>
2017-10-17 17:35:55 -0400

    refactor

--- src/main/scala/org/broadinstitute/dsde/agora/server/AgoraConfig.scala
+++ src/main/scala/org/broadinstitute/dsde/agora/server/AgoraConfig.scala
@@ -85,6 +85,12 @@ object AgoraConfig {
     lazy val appName = gcsProjectId
   }
 
+  // GA4GH
+  object GA4GH {
+    private lazy val baseGA4GHUrl = "%s://%s%s/ga4gh/%s/".format(scheme, host, embeddedUrlPortStr, version)
+    def toolUrl(id: String, versionId: String, toolType: String): String = baseGA4GHUrl + "tools/%s/versions/%s/%s/descriptor".format(id, versionId, toolType)
+  }
+
   def urlFromType(entityType: Option[EntityType]): String = {
     entityType match {
       case Some(AgoraEntityType.Task) | Some(AgoraEntityType.Workflow) => methodsUrl
@@ -113,12 +119,6 @@ object AgoraConfig {
         case Failure(e) => throw e
       }
     }
-  }
-
-  // GA4GH
-  object GA4GH {
-    private lazy val baseGA4GHUrl = scheme + "://" + host + embeddedUrlPortStr + "/" + "ga4gh" + "/" + version + "/"
-    def toolUrl(id: String, versionId: String, toolType: String): String = baseGA4GHUrl + "tools/%s/versions/%s/%s/descriptor".format(id, versionId, toolType)
   }
 
 }

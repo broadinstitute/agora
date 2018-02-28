b71e75f132179cd609e8722ac71546d6f7720f41
Greg Rushton <grushton@broadinstitute.org>
2018-02-28 15:52:44 -0500

    more cleanup

--- src/test/scala/org/broadinstitute/dsde/agora/server/webservice/Ga4ghServiceSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/agora/server/webservice/Ga4ghServiceSpec.scala
@@ -13,8 +13,6 @@ import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model.HttpMethods
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-//import akka.http.scaladsl.server.Directives
-//import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import DefaultJsonProtocol._
 import akka.http.scaladsl.server.directives.ExecutionDirectives
 

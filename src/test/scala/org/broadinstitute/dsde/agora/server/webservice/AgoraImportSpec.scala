e2d9a9c6fe52dd28f647c1e538dfe436f424d7fb
Kaan Yuksel <kyuksel@broadinstitute.org>
2018-04-04 13:56:10 -0400

    Refactored AgoraImportSpec

--- src/test/scala/org/broadinstitute/dsde/agora/server/webservice/AgoraImportSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/agora/server/webservice/AgoraImportSpec.scala
@@ -46,41 +46,16 @@ class AgoraImportSpec extends ApiServiceSpec with FlatSpecLike{
     mockServer.stop()
   }
 
-  // tests for creating new methods
-  "MethodsService" should "return 400 when posting a gibberish WDL" in {
-    Post(ApiUtil.Methods.withLeadingVersion, copyPayload(Some("this isn't valid WDL!"))) ~>
-      routes ~> check {
-      assert(status == BadRequest)
-      assert(responseAs[String] contains s"$errorMessagePrefix Unrecognized token")
-    }
-  }
+  private val createUrl = ApiUtil.Methods.withLeadingVersion
+  private val copyUrl = createUrl + s"/${testAgoraEntity.namespace.get}/${testAgoraEntity.name.get}/1"
 
-  "MethodsService" should "return a 400 when posting a WDL with an invalid import statement" in {
-    Post(ApiUtil.Methods.withLeadingVersion, testBadAgoraEntityInvalidWdlImportFormat) ~>
-      routes ~> check {
-      assert(status == BadRequest)
-      assert(responseAs[String] contains s"$errorMessagePrefix Failed to import workflow invalid_syntax_for_tool.:")
-    }
-  }
+  private val operationsByUrls = Map(
+    "posting" -> createUrl,
+    "copying" -> copyUrl)
 
-  "MethodsService" should "return a 400 when posting a WDL with an import statement that references a non-existent method" in {
-    Post(ApiUtil.Methods.withLeadingVersion, testBadAgoraEntityNonExistentWdlImportFormat) ~>
-      routes ~> check {
-      assert(status == BadRequest)
-      assert(responseAs[String] contains s"$errorMessagePrefix Failed to import workflow broad.non_existent_grep.1.:")
-    }
-  }
-
-  "MethodsService" should "return a 400 when posting a method and specifying a WDL with an import URL that returns a 404" in {
-    Post(ApiUtil.Methods.withLeadingVersion, copyPayload(testBadAgoraEntityWdlImportNotFound.payload)) ~>
-      routes ~> check {
-      assert(status == BadRequest)
-      assert(responseAs[String] contains s"$errorMessagePrefix Failed to import workflow http://localhost:$mockServerPort/not-found")
-    }
-  }
-
-  "MethodsService" should "return 201 when posting a valid WDL that contains an import" in {
-    Post(ApiUtil.Methods.withLeadingVersion, testEntityWorkflowWithExistentWdlImport) ~>
+  // Positive Tests
+  "MethodsService" should s"return 201 when posting a valid WDL that contains an import" in {
+    Post(createUrl, testEntityWorkflowWithExistentWdlImport) ~>
       routes ~> check {
       assert(status == Created)
       assert(responseAs[String] contains "\"name\":\"testMethod1\"")
@@ -88,51 +63,48 @@ class AgoraImportSpec extends ApiServiceSpec with FlatSpecLike{
     }
   }
 
-  // tests for copying a method and specifying new WDL for the copy
-
-  private val copyUrl = ApiUtil.Methods.withLeadingVersion +
-    s"/${testAgoraEntity.namespace.get}/${testAgoraEntity.name.get}/1"
-
-
-  "MethodsService" should "return 400 when copying a method and specifying a gibberish WDL" in {
-    Post(copyUrl, copyPayload(Some("this isn't valid WDL!"))) ~>
-      routes ~> check {
-        assert(status == BadRequest)
-        assert(responseAs[String] contains s"$errorMessagePrefix Unrecognized token")
-      }
-  }
-
-  "MethodsService" should "return a 400 when copying a method and specifying a WDL with an invalid import statement" in {
-    Post(copyUrl, copyPayload(testBadAgoraEntityInvalidWdlImportFormat.payload)) ~>
-      routes ~> check {
-        assert(status == BadRequest)
-        assert(responseAs[String] contains s"$errorMessagePrefix Failed to import workflow invalid_syntax_for_tool.:")
-      }
-  }
-
-  "MethodsService" should "return a 400 when copying a method and specifying a WDL with an import statement that references a non-existent method" in {
-    Post(copyUrl, copyPayload(testBadAgoraEntityNonExistentWdlImportFormat.payload)) ~>
-      routes ~> check {
-        assert(status == BadRequest)
-        assert(responseAs[String] contains s"$errorMessagePrefix Failed to import workflow broad.non_existent_grep.1.:")
-      }
-  }
-
-  "MethodsService" should "return a 400 when copying a method and specifying a WDL with an import URL that returns a 404" in {
-    Post(copyUrl, copyPayload(testBadAgoraEntityWdlImportNotFound.payload)) ~>
-      routes ~> check {
-        assert(status == BadRequest)
-        assert(responseAs[String] contains s"$errorMessagePrefix Failed to import workflow http://localhost:$mockServerPort/not-found")
-      }
-  }
-
   "MethodsService" should "return 200 when copying a method and specifying a valid WDL that contains an import" in {
     Post(copyUrl, copyPayload(testEntityWorkflowWithExistentWdlImport.payload)) ~>
       routes ~> check {
-        assert(status == OK)
-        assert(responseAs[String] contains "\"name\":\"testMethod1\"")
-        assert(responseAs[String] contains "\"createDate\"")
+      assert(status == OK)
+      assert(responseAs[String] contains "\"name\":\"testMethod1\"")
+      assert(responseAs[String] contains "\"createDate\"")
+    }
+  }
+
+  // Negative Tests
+  operationsByUrls foreach { case (processing, url) =>
+    "MethodsService" should s"return 400 upon $processing a gibberish WDL" in {
+      Post(url, copyPayload(Some("this isn't valid WDL!"))) ~>
+        routes ~> check {
+        assert(status == BadRequest)
+        assert(responseAs[String] contains s"$errorMessagePrefix Unrecognized token")
       }
+    }
+
+    "MethodsService" should s"return a 400 upon $processing a WDL with an invalid import statement" in {
+      Post(url, testBadAgoraEntityInvalidWdlImportFormat) ~>
+        routes ~> check {
+        assert(status == BadRequest)
+        assert(responseAs[String] contains s"$errorMessagePrefix Failed to import workflow invalid_syntax_for_tool.:")
+      }
+    }
+
+    "MethodsService" should s"return a 400 upon $processing a WDL with an import statement that references a non-existent method" in {
+      Post(url, testBadAgoraEntityNonExistentWdlImportFormat) ~>
+        routes ~> check {
+        assert(status == BadRequest)
+        assert(responseAs[String] contains s"$errorMessagePrefix Failed to import workflow broad.non_existent_grep.1.:")
+      }
+    }
+
+    "MethodsService" should s"return a 400 upon $processing a WDL with an import URL that returns a 404" in {
+      Post(url, copyPayload(testBadAgoraEntityWdlImportNotFound.payload)) ~>
+        routes ~> check {
+        assert(status == BadRequest)
+        assert(responseAs[String] contains s"$errorMessagePrefix Failed to import workflow http://localhost:$mockServerPort/not-found")
+      }
+    }
   }
 
   private def copyPayload(wdl:Option[String]) = testAgoraEntity.copy(payload=wdl)

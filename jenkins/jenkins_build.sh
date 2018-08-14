b1ec29b557b2b63d0a3b3eb17fc9cb6c907b4347
Jackie Roberti <jacqueline.roberti93@gmail.com>
2018-08-14 15:11:14 -0400

    vault token

--- jenkins/jenkins_build.sh
+++ jenkins/jenkins_build.sh
@@ -4,8 +4,9 @@ set -eux
 
 GCR_SVCACCT_VAULT="secret/dsde/dsp-techops/common/dspci-wb-gcr-service-account.json"
 GCR_REPO_PROJ="broad-dsp-gcr-public"
+VAULT_TOKEN=${VAULT_TOKEN:-$(cat /etc/vault-token-dsde)}
 
-docker run --rm  -v /etc/vault-token-dsde:/root/.vault-token:ro \
+docker run --rm  -e VAULT_TOKEN=$VAULT_TOKEN \
    broadinstitute/dsde-toolbox:latest vault read --format=json ${GCR_SVCACCT_VAULT} \
    | jq .data > dspci-wb-gcr-service-account.json
 

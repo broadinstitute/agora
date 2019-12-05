6ab9589981b48f7954bbb42a7bfa85a47161d2a8
Adam Nichols <aednichols@gmail.com>
2019-12-05 11:17:52 -0500

    KS suggestion

--- src/main/scala/org/broadinstitute/dsde/agora/server/model/AgoraEntityBsonSupport.scala
+++ src/main/scala/org/broadinstitute/dsde/agora/server/model/AgoraEntityBsonSupport.scala
@@ -1,7 +1,6 @@
 package org.broadinstitute.dsde.agora.server.model
 
 import com.mongodb.DBObject
-import org.bson.types.ObjectId
 import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
 
@@ -10,48 +9,37 @@ object AgoraEntityBsonSupport {
 
   def read(obj: DBObject): AgoraEntity =
     AgoraEntity(
-      namespace       = stringOrNone(obj, "namespace"),
-      name            = stringOrNone(obj, "name"),
-      snapshotId      = intOrNone(obj, "snapshotId"),
-      snapshotComment = stringOrNone(obj, "snapshotComment"),
-      synopsis        = stringOrNone(obj, "synopsis"),
-      documentation   = stringOrNone(obj, "documentation"),
-      owner           = stringOrNone(obj, "owner"), // Not in DB but checked by tests. Just rolling with this for now.
-      createDate      = dateOrNone(obj, "createDate"),
-      payload         = stringOrNone(obj, "payload"),
-      payloadObject   = None, // Not in DB
-      url             = None, // Not in DB
-      entityType      = entityType(obj),
-      id              = Option(new ObjectId(obj.get("_id").toString)),
-      methodId        = idOrNone(obj, "methodId"), // Configs only
-      method          = None, // Not in DB
-      managers        = Seq.empty, // Not in DB
-      public          = None, // Not in DB
+      namespace = getOrNone(obj, "namespace"),
+      name = getOrNone(obj, "name"),
+      snapshotId = getOrNone(obj, "snapshotId"),
+      snapshotComment = getOrNone(obj, "snapshotComment"),
+      synopsis = getOrNone(obj, "synopsis"),
+      documentation = getOrNone(obj, "documentation"),
+      owner = getOrNone(obj, "owner"), // Not in DB but checked by tests. Just rolling with this for now.
+      createDate = dateOrNone(obj, "createDate"),
+      payload = getOrNone(obj, "payload"),
+      payloadObject = None, // Not in DB
+      url = None, // Not in DB
+      entityType = entityType(obj),
+      id = getOrNone(obj, "_id"),
+      methodId = getOrNone(obj, "methodId"), // Configs only
+      method = None, // Not in DB
+      managers = Seq.empty, // Not in DB
+      public = None, // Not in DB
     )
 
-  private def idOrNone(obj: DBObject, key: String): Option[ObjectId] = {
-    if (obj.containsField(key)) Option(new ObjectId(obj.get(key).toString)) else None
-  }
-
-  private def stringOrNone(obj: DBObject, key: String): Option[String] = {
-    if (obj.containsField(key)) Option(obj.get(key).asInstanceOf[String]) else None
-  }
-
-  private def intOrNone(obj: DBObject, key: String): Option[Int] = {
-    if (obj.containsField(key)) Option(obj.get(key).asInstanceOf[Int]) else None
+  private def getOrNone[A](obj: DBObject, key: String): Option[A] = {
+    Option(obj.get(key).asInstanceOf[A])
   }
 
   private def dateOrNone(obj: DBObject, key: String): Option[DateTime] = {
-    if (obj.containsField(key)) Option(DateTime.parse(obj.get(key).asInstanceOf[String], ISODateTimeFormat.dateTimeNoMillis())) else None
+    val dateString = getOrNone[String](obj, key)
+    dateString.map(DateTime.parse(_, ISODateTimeFormat.dateTimeNoMillis()))
   }
 
   private def entityType(obj: DBObject): Option[AgoraEntityType.EntityType] = {
-    if (obj.containsField("entityType")) {
-      val entityString = Option(obj.get("entityType").asInstanceOf[String])
-      entityString.map(AgoraEntityType.withName)
-    } else {
-      None
-    }
+    val entityString = getOrNone[String](obj, "entityType")
+    entityString.map(AgoraEntityType.withName)
   }
 
 }

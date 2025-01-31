1e09979bdadbf2a74488848a8e633facef8a53bb
Saloni Shah <salonipshah11@gmail.com>
2025-01-31 15:41:35 +0530

    test 5.6.51

--- src/test/scala/org/broadinstitute/dsde/agora/server/dataaccess/permissions/AgoraPermissionsSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/agora/server/dataaccess/permissions/AgoraPermissionsSpec.scala
@@ -26,7 +26,7 @@ class AgoraPermissionsSpec extends ApiServiceSpec with BeforeAndAfterAll with Ag
     } yield version.head
     val version = runInDB { _ => action }
 
-    assert(version equals "8.0.40")
+    assert(version equals "5.6.51")
   }
 
   "Agora" should "return true if someone has read access and we ask if they have read access " in {
